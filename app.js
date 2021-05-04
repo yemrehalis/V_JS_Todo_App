883926b1d75395185eb72430271fb86d4784ae6a
Yunus Emre Halis <yemrehalis@gmail.com>
2021-05-04 23:45:54 +0300

    delete function added

--- app.js
+++ app.js
@@ -52,5 +52,15 @@ function addTodo(event){
 
 function  deleteCheck(e){
    const item = e.target;
-   console.log(item);
+   
+   // DELETE TODO
+   if(item.classList[0] === "trash-btn"){
+       const todo = item.parentElement;
+       todo.remove();
+   }
+
+   if(item.classList[0] === "complete-btn"){
+       const todo = item.parentElement;
+       todo.classList.toggle('completed');
+   }
 }
